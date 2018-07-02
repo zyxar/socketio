4d38dd97e64a72ab90011ebbc2d0a968bd482158
Markus Tzoe <chou.marcus@gmail.com>
2018-07-02 18:05:32 +0800

    socket.go

--- transport.go
+++ transport.go
@@ -1,7 +1,6 @@
 package engio
 
 import (
-	"encoding/json"
 	"io"
 	"net/http"
 )
@@ -15,58 +14,4 @@ type Conn interface {
 	NextReader() (MessageType, PacketType, io.ReadCloser, error)
 	NextWriter(MessageType, PacketType) (io.WriteCloser, error)
 	Close() error
-}
-
-type Socket struct {
-	Conn
-	*eventHandlers
-}
-
-func (s *Socket) Handle() error {
-	return s.eventHandlers.handle(s)
-}
-
-func (s *Socket) Emit(event string, args interface{}) (err error) {
-	var pktType PacketType
-	switch event {
-	case EventOpen:
-		pktType = PacketTypeOpen
-	case EventMessage:
-		pktType = PacketTypeMessage
-	case EventClose:
-		pktType = PacketTypeClose
-	// case EventError:
-	// case EventUpgrade:
-	case EventPing:
-		pktType = PacketTypePing
-	case EventPong:
-		pktType = PacketTypePong
-	default:
-		err = ErrInvalidEvent
-		return
-	}
-	data, err := json.Marshal(args)
-	if err != nil {
-		return
-	}
-	err = send(s.Conn, MessageTypeString, pktType, data)
-	return
-}
-
-func (s *Socket) Send(args interface{}) (err error) {
-	return s.Emit(EventMessage, args)
-}
-
-func send(conn Conn, msgType MessageType, pktType PacketType, data []byte) (err error) {
-	wc, err := conn.NextWriter(msgType, pktType)
-	if err != nil {
-		return
-	}
-	if len(data) > 0 {
-		if _, err = wc.Write(data); err != nil {
-			wc.Close()
-			return
-		}
-	}
-	return wc.Close()
 }
