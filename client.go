aa191462ba6e78ced0b8a3cba83dffb9c314585f
Markus Tzoe <chou.marcus@gmail.com>
2018-07-06 00:50:32 +0800

    session.go

--- client.go
+++ client.go
@@ -44,7 +44,7 @@ func Dial(rawurl string, requestHeader http.Header, dialer Dialer) (c *Client, e
 	pingTimeout := time.Duration(param.PingTimeout) * time.Millisecond
 
 	closeChan := make(chan struct{}, 1)
-	so := &Socket{conn, newEventHandlers(), pingTimeout, pingTimeout}
+	so := &Socket{Conn: conn, eventHandlers: newEventHandlers(), readTimeout: pingTimeout, writeTimeout: pingTimeout}
 	c = &Client{
 		Socket:       so,
 		pingInterval: pingInterval,
--- server.go
+++ server.go
@@ -1,9 +1,6 @@
 package engio
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"io"
 	"net/http"
 	"sync"
 	"time"
@@ -103,67 +100,4 @@ func (s *Server) BindAndListen(srv *http.Server) error {
 	}
 	srv.Handler = s
 	return srv.ListenAndServe()
-}
-
-type session struct {
-	*Socket
-	id string
-}
-
-func (s *session) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if handler, ok := s.Socket.Conn.(http.Handler); ok {
-		handler.ServeHTTP(w, r)
-	}
-}
-
-func newSession(conn Conn, readTimeout, writeTimeout time.Duration) *session {
-	id := generateRandomKey(24)
-	return &session{
-		Socket: &Socket{
-			Conn:          conn,
-			eventHandlers: newEventHandlers(),
-			readTimeout:   readTimeout,
-			writeTimeout:  writeTimeout},
-		id: base64.StdEncoding.EncodeToString(id),
-	}
-}
-
-type sessionManager struct {
-	ß map[string]*session
-	sync.RWMutex
-}
-
-func newSessionManager() *sessionManager {
-	return &sessionManager{
-		ß: make(map[string]*session),
-	}
-}
-
-func (s *sessionManager) Get(id string) (ß *session, b bool) {
-	s.RLock()
-	ß, b = s.ß[id]
-	s.RUnlock()
-	return
-}
-
-func (s *sessionManager) Remove(id string) {
-	s.Lock()
-	delete(s.ß, id)
-	s.Unlock()
-}
-
-func (s *sessionManager) NewSession(conn Conn, readTimeout, writeTimeout time.Duration) *session {
-	ß := newSession(conn, readTimeout, writeTimeout)
-	s.Lock()
-	s.ß[ß.id] = ß
-	s.Unlock()
-	return ß
-}
-
-func generateRandomKey(length int) []byte {
-	k := make([]byte, length)
-	if _, err := io.ReadFull(rand.Reader, k); err != nil {
-		return nil
-	}
-	return k
 }
